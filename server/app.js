6a3df50a847e9534cd05dfe7edbc28cf85140184
sara faltas <gadly@cloudconceptgroup.com>
2026-05-13 17:30:41 +0200

    mergeed

--- server/app.js
+++ server/app.js
@@ -2,11 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-
-// STATIC DATA
-// Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
-
+const Cohort = require("./models/Cohort.model");
+const Student = require("./models/Student.model");
 
 
 
@@ -17,9 +14,14 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-
 const cors = require("cors")
 app.use(cors());
+
+//connect to the DB 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
 
 
 app.use(express.json());
@@ -29,21 +31,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
-// ROUTES - https://expressjs.com/en/starter/basic-routing.html
+// ROUTES -
 // Devs Team - Start working on the routes here:
 // ...
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
-const cohort = require("./cohorts.json")
-app.get("/api/cohorts", (req, res) => {
-  res.json(cohort)
+app.get("/cohort", (req, res) => {
+  Cohort.find()
+    .then((cohorts) => {
+      console.log("Retrieved cohorts ->", cohorts);
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving cohorts ->", error);
+      res.status(500).json({ error: "Failed to retrieve books" });
+    });
 });
 
-const students = require("./students.json")
-app.get("/api/students", (req, res) => {
-  res.json(students)
+app.get("/students", (req, res) => {
+  Student.find({})
+    .then((cohorts) => {
+      console.log("Retrieved students ->", students);
+      res.json(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving students ->", error);
+      res.status(500).json({ error: "Failed to retrieve books" });
+    });
 });
 
 
