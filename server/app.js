b859cb9e877d5ed78b6986fa151fc0ff1bcf459f
Dante Besong <dantebesong321@gmail.com>
2026-05-13 17:45:24 +0200

    in-progress

--- server/app.js
+++ server/app.js
@@ -1,35 +1,30 @@
 const express = require("express");
-const morgan = require("morgan");
+const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const Cohort = require("./models/Cohort.model");
 const Student = require("./models/Student.model");
 
-
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-const cors = require("cors")
+const cors = require("cors");
 app.use(cors());
 
-//connect to the DB 
+//connect to the DB
 mongoose
   .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
-  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
-  .catch(err => console.error("Error connecting to MongoDB", err));
-
+  .then((x) => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to MongoDB", err));
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 
 // ROUTES -
 // Devs Team - Start working on the routes here:
@@ -61,7 +56,6 @@ app.get("/students", (req, res) => {
       res.status(500).json({ error: "Failed to retrieve books" });
     });
 });
-
 
 // START SERVER
 app.listen(PORT, () => {
